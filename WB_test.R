5bb0dd1e3168b08f3c40d786852da2da5f4af275
Xinhu Wang <xw2439@columbia.edu>
2024-04-24 20:21:03 -0400

    Updated my answers on Q2

--- WB_test.R
+++ WB_test.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(janitor)
 library(readr)
 library(dplyr)
 library(car)
@@ -20,18 +21,47 @@ gdp_per_cap <- gdp_long %>% inner_join(pop_long, by = c("Country Name", "Country
 # Question 2: regional GDP with weighted average
 # Detected error in WB_groups.csv, filter for AFW and AFE regions before merging
 regional_groups <- wb_groups %>% filter(GroupCode %in% c("AFW", "AFE"))
-gdp_af <- gdp_per_cap %>% inner_join(regional_groups, by = c("Country Code" = "CountryCode"))
-# Calculate regional population and regional GDP
-gdp_regional <- gdp_af %>% group_by(year, GroupCode) %>% 
-                            summarise(RegionalPop = sum(Population),RegionalGDP = sum(GDP)) %>% 
-                            mutate(`Avg GDP per capita` = `RegionalGDP`/`RegionalPop`)
+gdp_af <- gdp_per_cap %>% inner_join(regional_groups, by = c("Country Code" = "CountryCode")) 
+gdp_af$year <- as.numeric(gdp_af$year)
 
-# Visulization using ggplot
-plot_q2 <- ggplot(data = gdp_regional, aes(x = year, y = `Avg GDP per capita`, group = GroupCode, color = GroupCode)) + 
-  geom_line() +
+# take a look at summary stats and data coverage, based on the data coverage, I decided
+# to only use data after 1984 (inclusive) for visualizations.
+# For further explanation, please see README.md
+summary(gdp_af)
+gdp_af %>% tabyl(year, GroupName)
+
+gdp_af_sub <- gdp_af %>% subset(year >= 1984)
+
+# Calculate regional population and regional GDP
+gdp_regional <- gdp_af_sub %>% group_by(year, GroupCode) %>% 
+  summarise(RegionalPop = sum(Population),RegionalGDP = sum(GDP)) %>% 
+  mutate(`Avg GDP per capita` = `RegionalGDP`/`RegionalPop`)
+
+# Visualization - trends 1960-2022
+plot_trend <- ggplot(data = gdp_regional, aes(x = year, y = `Avg GDP per capita`, group = GroupCode, color = GroupCode)) + 
+  geom_line() + scale_x_continuous(breaks=seq(1984,2022,1)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
-  labs(title = "Average GDP per capita for AFW and AFE regions, 1960-2022")
-plot_q2
+  labs(x = '',title = "Average GDP per capita for AFW and AFE regions, 1984-2022")
+plot_trend
+
+# Visualization - difference between afe and afw
+gdp_diff <- gdp_regional %>% group_by(year) %>% summarise(GDP_diff = diff(`Avg GDP per capita`))
+plot_diff <- ggplot(data = gdp_diff, aes(x = year, y = GDP_diff)) + 
+  geom_bar(stat = "identity") + scale_x_continuous(breaks=seq(1984, 2022, 1)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = '', y = "Difference in Average GDP (AFW-AFE)", title = "Average GDP per capita Differences between AFW and AFE regions, 1984-2022",
+       subtitle = "Difference = GDP value of AFW - GDP value of AFE")
+plot_diff
+
+# Visualization for AFW
+plot_afw <- ggplot() + 
+  geom_line(data = subset(gdp_af, GroupCode == "AFW"), aes(x = year, y = `GDP per capita`, group = `Country Code`, color = `Country Code`)) + 
+  geom_line(data = subset(gdp_regional, GroupCode == "AFW"), aes(x = year, y = `Avg GDP per capita`), color = 'black', size = 0.8) +
+  geom_text(data = subset(gdp_regional, GroupCode == "AFW" & year == 2022), aes(label = 'AFW Region', x = 2025, y = `Avg GDP per capita`), colour = "black") + 
+  scale_y_continuous(breaks=seq(0,20000,2000)) + scale_x_continuous(breaks=seq(1960, 2022, 1)) +
+  labs(x = '', y = "GDP per capita", title = "GDP per capita in AFW and Regional Average, 1960-2022") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+plot_afw
 
 # Question 3: Please see ReadMe.md
 
